97852910673209e11802b69ccd090c1f19298795
stoiclunchbox <dsimp14@gmail.com>
2023-04-09 00:08:36 +1000

    tachymeter: inhibit timeout when running

--- movement/watch_faces/complication/tachymeter_face.c
+++ movement/watch_faces/complication/tachymeter_face.c
@@ -250,7 +250,7 @@ bool tachymeter_face_loop(movement_event_t event, movement_settings_t *settings,
         case EVENT_TIMEOUT:
             // Your watch face will receive this event after a period of inactivity. If it makes sense to resign,
             // you may uncomment this line to move back to the first watch face in the list:
-            movement_move_to_face(0);
+            if (!state->running) movement_move_to_face(0);
             break;
         case EVENT_LOW_ENERGY_UPDATE:
             // If you did not resign in EVENT_TIMEOUT, you can use this event to update the display once a minute.
