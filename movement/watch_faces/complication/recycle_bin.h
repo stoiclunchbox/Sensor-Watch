20ac3f322843ff1a0a63805d4a4b0482ca3e4225
stoiclunchbox <dsimp14@gmail.com>
2023-04-09 15:22:30 +1000

    add recycle_bin face (wip)

---
+++ movement/watch_faces/complication/recycle_bin.h
@@ -0,0 +1,55 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) <#year#> <#author_name#>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef recycle_bin_FACE_H_
+#define recycle_bin_FACE_H_
+
+#include "movement.h"
+
+/*
+ * A DESCRIPTION OF YOUR WATCH FACE
+ *
+ * and a description of how use it
+ *
+ */
+
+typedef struct {
+    watch_date_time datetime_start;
+} recycle_bin_state_t;
+
+void recycle_bin_face_setup(movement_settings_t *settings, uint8_t watch_face_index, void ** context_ptr);
+void recycle_bin_face_activate(movement_settings_t *settings, void *context);
+bool recycle_bin_face_loop(movement_event_t event, movement_settings_t *settings, void *context);
+void recycle_bin_face_resign(movement_settings_t *settings, void *context);
+
+#define recycle_bin_face ((const watch_face_t){ \
+    recycle_bin_face_setup, \
+    recycle_bin_face_activate, \
+    recycle_bin_face_loop, \
+    recycle_bin_face_resign, \
+    NULL, \
+})
+
+#endif // recycle_bin_FACE_H_
+
